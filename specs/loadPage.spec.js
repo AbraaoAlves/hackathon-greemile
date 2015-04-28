021130838ff26d42a4902057a76dbdbcd2cb6994
abraaoalves <abraao.teodosio@gmail.com>
2015-04-27 22:37:06 -0300

    test(index.html): page should have init with empty tbody and filled by automatic request result issue #5

--- specs/loadPage.spec.js
+++ specs/loadPage.spec.js
@@ -1,9 +1,32 @@
+/// get start with protractor: http://angular.github.io/protractor/#/tutorial
 
-// spec.js
-describe('Load Page spec', function() {
-  it('should have a title', function() {
+describe('index.html page ', function() {
+  beforeEach(function(){
     browser.get('http://localhost:8080/');
 
+  })
+  
+  it('should have a title', function() {
     expect(browser.getTitle()).toEqual('Hackathon Greenmile');
   });
+
+  it('should have empty tbody', function(cb){
+	var tbody = $('table tbody');
+
+	tbody.getInnerHtml().then(function  (html) {
+		expect(html).toBe('');
+		cb();
+	});
+  });
+
+  it('should have filled tbody after 3 seconds', function(cb){
+	var tbody = $('table tbody');
+	 browser.sleep(3000);
+
+	tbody.getInnerHtml().then(function  (html) {
+		expect(html).not.toBe('');
+		cb();
+	});
+  });
+
 });
