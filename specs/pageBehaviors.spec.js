2e0d88c74def2c5654837430d2c3e23e728f4d75
abraaoalves <abraao.teodosio@gmail.com>
2015-04-27 23:02:54 -0300

    test(index): when click in team without school, map should be same #5

--- specs/pageBehaviors.spec.js
+++ specs/pageBehaviors.spec.js
@@ -18,4 +18,18 @@ describe('index.html behaviors', function() {
 			});
 		});
     });
+
+    it('if show message: "Este time não possui escolas", map not should change', function(cb) {
+		$('#map').getInnerHtml().then(function  (initMapHtml) {
+			
+			$('tbody tr:nth-child(2)').click();
+    		browser.sleep(3000); //wait load data
+			
+			$('#map').getInnerHtml().then(function  (actualMapHtml) {
+
+				expect(initMapHtml).toEqual(actualMapHtml);
+				cb();
+			});
+		});
+    });
 });
