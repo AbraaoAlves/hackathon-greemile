2c559e6af082581b164bac1b25c819a0a81f8e89
abraaoalves <abraao.teodosio@gmail.com>
2015-05-02 05:00:11 -0300

    set paralel problem on run webdriver and protractor

--- gulpfile.js
+++ gulpfile.js
@@ -4,18 +4,19 @@ var sequence = require('run-sequence');
 var exit = require('gulp-exit');
 
 gulp.task('webdriver-start', shell.task('webdriver-manager start'));
-gulp.task('protractor-start', shell.task('protractor protractor.config.js'));
+gulp.task('e2e',shell.task('protractor protractor.config.js'));
 
-gulp.task('e2e', function(cb){
-	sequence(
-		'webdriver-start',
-		'protractor-start',
-		function() {
-	      gulp.src("").pipe(exit());
-	      cb();
-		}
-	);
-});
+//incomplete
+//gulp.task('e2e', function(cb){
+//	//TODO: check if webdriver is runnig 
+//	sequence(
+//		['webdriver-start', 'protractor-start'],
+//		function() {
+//	      gulp.src("").pipe(exit());
+//	      cb();
+//		}
+//	);
+//});
 
 gulp.task('watch-e2e', function(){
 	gulp.watch(['specs/*spec.js', 'app/assets/js/*.js'], ['e2e']);
